2ea9cf7ff95d8149c5bef18a27da4aed6a3800a7
lucasfemar <lucasdeveloper98l@gmail.com>
2024-05-08 22:29:54 -0300

    Remove `sequelize` e `postgres`

--- src/infra/database.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-async function getSequelizeConnection() {
-    const sequelize = new Sequelize(process.env.DATABASE_URL);
-    return sequelize;
-}
-
-export { getSequelizeConnection };
